2af0b79d3dc286d6a138c0efdd98052ec6cfef4e
victhangnguyen <victhangnguyen@outlook.com>
2024-07-20 02:46:22 +0700

    Initialize files and folder constructure, move page to (base-layout), tailwind config update colors theme/extend/colors, create Meta Component in utils

---
+++ src/app/(base-layout)/page.tsx
@@ -0,0 +1,16 @@
+import Meta from "@/utils/Meta";
+
+export default function Home() {
+  return (
+    <div>
+      <Meta
+        title="EduSphere"
+        description="Discover EduSphere: Your Key to Convenient and Engaging Online Learning. Explore a Diverse Range of Courses and Empower Your Educational Journey with Our User-Friendly, Modern eLearning Platform."
+        keywords="Online Learning, E-Learning, Skill Development, Programming, ReactJs, NodeJs, MERN"
+      />
+      <main>
+        <p>Hello world!</p>
+      </main>
+    </div>
+  );
+}
